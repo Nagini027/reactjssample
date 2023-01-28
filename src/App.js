c13a8e9ee155c6321922db69206bb3242ef3847a
Nagini027 <nagalakshmis027@gmail.com>
2023-01-28 20:06:37 +0530

    updated files

--- src/App.js
+++ src/App.js
@@ -1,25 +1,58 @@
-import logo from './logo.svg';
-import './App.css';
+import Header from './components/layout/header';
+import { useState} from 'react';
+import Meals from './components/meals/Meals';
+import Cart from './components/cart/Cart';
+import CartProvider from './store/CartProvider';
+function App(){
+ 
+  const [cartIsShown, setCartIsShown] = useState(false);
+  const showCartHandler = () => {
+    setCartIsShown(true);
+  };
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+  const hideCartHandler = () => {
+    setCartIsShown(false);
+  };
+  return(
+  // <div>
+  //    <h1>Let's get Started</h1>
+  // </div>
+  
+
+      <CartProvider>
+       {cartIsShown && <Cart onClose={hideCartHandler} />}
+        <Header onShowCart={showCartHandler}  />
+        <main>
+        <Meals/>
+        </main>
+  </CartProvider>
   );
 }
+// export default App;
+// import React from "react";
+// import Demo from "./Demo";
+// class App extends React.Component {
+//   constructor(props) {
+//     super(props);
+//     this.state = {
+//       name: "Nagis"
+//     }
+//   }
+
+//   static getDerivedStateFromProps(props, state) {
+//     console.log("get derived state fom props", state);
+
+//     return null;
+//   }
+
+//   render() {
+//     return(
+//       <div>
+//         <p>Parent</p>
+//         <Demo name={this.state.name}/>
+//       </div>
+//     )
+//   }
+// }
 
 export default App;
